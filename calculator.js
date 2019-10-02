764ead273a4dcf177d0a52ff658b15099edea10b
Ethan OBrien <ethanobrien1009@gmail.com>
2019-10-03 11:38:53 +1300

    Add pseudocode for calc java

---
+++ calculator.js
@@ -0,0 +1,35 @@
+// create an entries array to store input numbers
+
+//create a total this is what the answer will be equal too.
+
+// create a var called temp. temp should show the numbers you are selecting temporarily e.g 3 shows 3 the press + and 4 shows either 4/7
+
+//Get a number add it to temp var
+
+//got a symbol other than equals add temp to entries var
+
+//if AC clear temp and entries
+
+//if CE clear teh last entry(number in temp)
+
+//change multiple symbol to multiply temp, push into entries and then clear temp
+
+//change divide symbol  to divide temp, push into entries and then clear temp
+
+//got equals sign, preform big calculation
+
+  //push temp
+  //create new var called nt 
+  //equal to number function running entries
+  //run a for loop
+  //create to new vars to store the number entries
+  //create new var to store symbols from entries
+
+  //if symbol === - make nt -= nextNum
+  //etc for other symbols
+
+  // create increment i++ so it runs through until end.
+
+  // swap - symbol so input can handle negative numbered answeres
+
+  //push number val
